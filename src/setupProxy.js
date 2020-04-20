efdb744cb1c8b126d0d46b0143bc16740d377517
AdamDoquiatan <adamdoq@gmail.com>
2020-04-20 16:17:17 -0700

    proxy/hosting troubleshooting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
 	console.log('Setup proxy is called')
-	app.use(createProxyMiddleware('/api', { target: 'http://localhost:6000/' }))
+	app.use(createProxyMiddleware('/api', { target: 'http://localhost:6000/', changeOrigin: true }))
 }
