657fffa9f23f75cd7a4d109ffd707f6b851bf5d5
AdamDoquiatan <adamdoq@gmail.com>
2020-04-20 17:18:15 -0700

    moved frontend to client directory + created seperate package.json for backend

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,5 +2,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
 	console.log('Setup proxy is called')
-	app.use(createProxyMiddleware('/api', { target: 'https://localhost:6000/', changeOrigin: true }))
+	app.use(createProxyMiddleware('/api', { target: 'http://localhost:6000/', changeOrigin: true }))
 }
