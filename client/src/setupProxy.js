f595396bd27fea75d3d64b677b278df0872f1504
AdamDoquiatan <adamdoq@gmail.com>
2020-04-20 18:06:22 -0700

    diff proxy target based on prod/dev build

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,5 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
 	console.log('Setup proxy is called')
-	app.use(createProxyMiddleware('/api', { target: 'https://localhost:6000/', changeOrigin: true }))
+	if (process.env.NODE_ENV === 'production') {
+		app.use(createProxyMiddleware('/api', { target: 'https://localhost:6000/', changeOrigin: true }))
+	} else {
+		app.use(createProxyMiddleware('/api', { target: 'http://localhost:6000/', changeOrigin: true }))
+	}
 }
