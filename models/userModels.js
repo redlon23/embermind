51506bbbf48b2753f8656da6ebd0469ddfcf6d75
tanika <tanikacharles@gmail.com>
2020-04-22 15:20:59 -0700

    login in userModels

--- models/userModels.js
+++ models/userModels.js
@@ -3,23 +3,11 @@ const User = require('../models/user');
 exports.registerNewUser = async (req) => {
 	var user = new User(req);
 	var result = await user.save();
-	console.log(result);
 	return result;
-    //return await user.save(function (err, obj) {
-	//	if (err) throw err;
-	//	//else user is registered
-	//	console.log("models" + obj);
-	//	//return obj ?
-	//}).obj;
 };
 
 exports.loginUser = async (loginCreds) => {
 	var query  = User.where({ email: loginCreds.email, password: loginCreds.password });
-	query.findOne(function (err, user) {
-  		if (err) throw err;
-  		if (user) {
-			//save in session?
-			return user;
-  		}
-	});
+	var result = query.findOne();
+	return result;
 }
