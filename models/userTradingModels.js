57292a430cfa356d8e27ffd782223646b1ac60d5
tanika <tanikacharles@gmail.com>
2020-04-27 13:54:29 -0700

    db update strategy settings

--- models/userTradingModels.js
+++ models/userTradingModels.js
@@ -12,3 +12,28 @@ exports.insertStrategySetting = async (req) => {
 		return result
 	}
 };
+
+exports.updateStrategySetting = async (req) => {
+	try{
+        var result = UserStrategySetting.update({ userId: req.userId, strategyId: req.strategyId }, 
+            { $set:{ 
+                contractQuantity: req.contractQuantity,
+                takeProfit: req.takeProfit,
+                tradeInterval: req.tradeInterval,
+                maxContractSize: req.maxContractSize,
+                DCA: req.DCA,
+                numOrders: req.numOrders,
+                spread: req.spread,
+                orderSpread: req.orderSpread,
+                trailingSafety: req.trailingSafety,
+                trailingStop: req.trailingStop,
+                noTradingZoneSize: req.noTradingZoneSize,
+                noTradingZoneRange: req.noTradingZoneRange
+                }}).exec();
+	} catch(err) {
+		console.log(err);
+		result = null;
+	} finally {
+		return result;
+	}
+}
