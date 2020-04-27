7a9bb4d6e6dd71f0b7b580bb0d4a057ceef9c2f6
AdamDoquiatan <adamdoq@gmail.com>
2020-04-26 20:21:27 -0700

    SideNavBar highlights current page + added switch to toggle automation

--- client/src/modules/SideNavBar/SideNavBar.js
+++ client/src/modules/SideNavBar/SideNavBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import './SideNavBar.css'
 
-import { Layout, Menu, Avatar, Badge, Button, Typography } from 'antd'
+import { Layout, Menu, Avatar, Badge, Button, Switch } from 'antd'
 import { BellOutlined } from '@ant-design/icons'
 
 const { Sider } = Layout
@@ -28,26 +28,31 @@ class SideNavBar extends Component {
 							/>
 						</Badge>
 					</div>
-					<Menu theme="dark" mode="inline" defaultSelectedKeys={[ '1' ]}>
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={[ this.props.location.pathname ]}>
 						<Menu.Divider />
-						<Menu.Item key="1" onClick={() => this.props.history.push('/dashboard')}>
+						<Menu.Item key="/dashboard" onClick={() => this.props.history.push('/dashboard')}>
 							<span className="nav-text">Dashboard</span>
 						</Menu.Item>
-						<Menu.Item key="2">
+						<Menu.Item key="/trade-records" disabled={true}>
 							<span className="nav-text">Trade Records</span>
 						</Menu.Item>
-						<Menu.Item key="3">
+						<Menu.Item key="/equipped-strategies" disabled={true}>
 							<span className="nav-text">Equipped Strategies</span>
 						</Menu.Item>
 						<Menu.Divider />
-						<Menu.Item key="4" onClick={() => this.props.history.push('/account-settings')}>
+						<Menu.Item key="/account-settings" onClick={() => this.props.history.push('/account-settings')}>
 							<span className="nav-text">Account Settings</span>
 						</Menu.Item>
-						<Menu.Item key="5" onClick={() => this.props.history.push('/strategy-settings')}>
+						<Menu.Item key="/strategy-settings" onClick={() => this.props.history.push('/strategy-settings')}>
 							<span className="nav-text">Strategy Settings</span>
 						</Menu.Item>
-						<Menu.Item key="6">
-							<span className="nav-text">Active / Inactive</span>
+					</Menu>
+					<Menu theme="dark" selectable={false}>
+						<Menu.Item>
+							<Switch defaultChecked={false} />
+							<span className="nav-text" style={{ paddingLeft: '0.6rem' }}>
+								Active / Inactive
+							</span>
 						</Menu.Item>
 					</Menu>
 				</Sider>
