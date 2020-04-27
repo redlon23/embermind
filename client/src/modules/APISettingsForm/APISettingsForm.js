d599bbbfd9c9e1bbbf77f3757f8ddd4b1b2cf0e8
AdamDoquiatan <adamdoq@gmail.com>
2020-04-27 14:33:41 -0700

    UI for UserSettingsModule

---
+++ client/src/modules/APISettingsForm/APISettingsForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Form, Input, Button } from 'antd'
+
+const contentStyle = {
+	background: '#1A1C25',
+	height: '19.5rem',
+	fontSize: '16pt',
+	padding: '1rem',
+	color: '#EBEBEB'
+}
+
+const layout = {
+	labelCol: {
+		span: 4
+	},
+	wrapperCol: {
+		span: 16
+	},
+	style: { padding: '1rem' }
+}
+
+class APISettingsForm extends Component {
+	render() {
+		return (
+			<div style={{ ...contentStyle }}>
+				API Settings
+				<Form className="form-section" {...layout} size={'small'}>
+					<Form.Item className="form-group" label="Name" name="Name" rules={[ { required: true } ]}>
+						<Input />
+					</Form.Item>
+
+					<Form.Item className="form-group" label="Email" name="Email" rules={[ { required: true } ]}>
+						<Input />
+					</Form.Item>
+
+					<Form.Item className="form-group" label="Password" name="Password" rules={[ { required: true } ]}>
+						<Input.Password />
+					</Form.Item>
+
+					<Form.Item>
+						<Button className="form-group" type="primary" htmlType="submit">
+							Submit
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
+		)
+	}
+}
+
+export default APISettingsForm
--- client/src/pages/AccountSettingsPage/AccountSettingsPage.js
+++ client/src/pages/AccountSettingsPage/AccountSettingsPage.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
-
 import HeaderNavBar from '../../modules/HeaderNavBar/HeaderNavBar'
 import SideNavBar from '../../modules/SideNavBar/SideNavBar'
+import UserSettingsForm from '../../modules/UserSettingsForm/UserSettingsForm'
+import APISettingsForm from '../../modules/APISettingsForm/APISettingsForm'
 
-import { Row, Col, Layout, Typography } from 'antd'
+import { Row, Col, Layout } from 'antd'
 
 const { Content } = Layout
-const { Title } = Typography
 
-const contentStyle = { background: '#EBEBEB', border: '2px dashed blue', height: '19.5rem' }
-const contentGutter = [ 4, 4 ]
 const headerStyle = {
 	height: '3rem',
 	backgroundColor: '#1A1C25',
 	color: '#EBEBEB',
 	display: 'flex',
 	alignItems: 'center',
-	fontSize: '18pt',
+	fontSize: '16pt',
 	padding: '1rem'
 }
+
+const contentStyle = {
+	background: '#1A1C25',
+	height: '19.5rem',
+	fontSize: '16pt',
+	padding: '1rem',
+	color: '#EBEBEB'
+}
+const contentGutter = [ 4, 4 ]
 
 class AccountSettingsPage extends Component {
 	render() {
@@ -32,25 +39,23 @@ class AccountSettingsPage extends Component {
 							<Content style={{ padding: '2rem 2rem 0rem 2rem' }}>
 								<Row gutter={[ 28, { xs: 10, sm: 18, md: 26, lg: 34 } ]}>
 									<Col span={24}>
-										<div className="header-bar" style={headerStyle}>
-											Account Settings
-										</div>
+										<div style={headerStyle}>Account Settings</div>
 									</Col>
 								</Row>
 								<Row gutter={contentGutter}>
 									<Col span={12}>
-										<div style={{ ...contentStyle }}>Content 1</div>
+										<UserSettingsForm />
 									</Col>
 									<Col span={12}>
-										<div style={{ ...contentStyle }}>Content 2</div>
+										<APISettingsForm />
 									</Col>
 								</Row>
 								<Row gutter={contentGutter}>
 									<Col span={12}>
-										<div style={{ ...contentStyle }}>Content 3</div>
+										<div style={{ ...contentStyle }}>Subscription Details</div>
 									</Col>
 									<Col span={12}>
-										<div style={{ ...contentStyle }}>Content 3</div>
+										<div style={{ ...contentStyle }}>Purchase Subscription</div>
 									</Col>
 								</Row>
 							</Content>
