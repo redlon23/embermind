4a44124999b749a0d82f36aa471271fc95136202
AdamDoquiatan <adamdoq@gmail.com>
2020-05-03 15:04:53 -0700

    StrategyCard UI Elements

--- client/src/pages/BrowseStrategiesPage/BrowseStrategiesPage.js
+++ client/src/pages/BrowseStrategiesPage/BrowseStrategiesPage.js
@@ -22,9 +22,33 @@ class BrowseStrategiesPage extends Component {
 		super(props)
 		this.state = {
 			strategies: [
-				{ title: 'Epic Trades X', description: 'A good strat!', avgProfitPerTrade: 0.025, subscriberCount: 44, avgRating: 4 },
-				{ title: 'Crypto Gnome', description: 'An even better strat!', avgProfitPerTrade: 0.06, subscriberCount: 300, avgRating: 5 },
-				{ title: 'Crypto Gnome', description: 'An even better strat!', avgProfitPerTrade: 0.06, subscriberCount: 300, avgRating: 5 }
+				{
+					title: 'Epic Trades X',
+					description: 'A good strat!',
+					avgProfitPerTrade: 0.025,
+					subscriberCount: 44,
+					ratingCount: 14,
+					avgRating: 4,
+					imgPath: 'https://cdn.wallpapersafari.com/47/75/i8cgUE.jpg'
+				},
+				{
+					title: 'Shrek Strat',
+					description: 'Somebody once told me the world was gonna roll me',
+					avgProfitPerTrade: 0.06,
+					subscriberCount: 300,
+					ratingCount: 124,
+					avgRating: 4.5,
+					imgPath: 'https://uploads.scratch.mit.edu/users/avatars/34083956.png'
+				},
+				{
+					title: 'Crypto Bob',
+					description: 'Obviously the best strat!',
+					avgProfitPerTrade: 0.1,
+					subscriberCount: 2500,
+					ratingCount: 1101,
+					avgRating: 5,
+					imgPath: 'https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199879943/Ramen-Panda_800x800_SEPS-500x500.jpg'
+				}
 			]
 		}
 	}
@@ -36,17 +60,12 @@ class BrowseStrategiesPage extends Component {
 
 	cardRow = (strategy, index) => (
 		<Row gutter={[ 28, 16 ]}>
-			<Col span={8}>
+			<Col span={12}>
 				<BrowseStrategiesCard {...strategy} />
 			</Col>
 			{this.state.strategies[index + 1] ? (
-				<Col span={8}>
+				<Col span={12}>
 					<BrowseStrategiesCard {...this.state.strategies[index + 1]} />
-				</Col>
-			) : null}
-			{this.state.strategies[index + 2] ? (
-				<Col span={8}>
-					<BrowseStrategiesCard {...this.state.strategies[index + 2]} />
 				</Col>
 			) : null}
 		</Row>
@@ -66,7 +85,7 @@ class BrowseStrategiesPage extends Component {
 								</Col>
 							</Row>
 							<div style={contentStyle}>
-								{this.state.strategies.map((strategy, index) => (index % 3 === 0 ? this.cardRow(strategy, index) : null))}
+								{this.state.strategies.map((strategy, index) => (index % 2 === 0 ? this.cardRow(strategy, index) : null))}
 							</div>
 						</Content>
 					</Layout>
--- client/src/pages/BrowseStrategiesPage/modules/BrowseStrategiesCard.js
+++ client/src/pages/BrowseStrategiesPage/modules/BrowseStrategiesCard.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
-import { Card, Avatar } from 'antd'
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons'
+import { Card, Rate, Row, Col, Button } from 'antd'
 
 const { Meta } = Card
 
 class BrowseStrategiesCard extends Component {
 	render() {
 		return (
-			<div className="BrowseStategiesCard" style={{ display: 'flex', flexDirection: 'row' }}>
-				<img src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{ width: '150px' }} />
-				<Card title="Default size card" style={{ width: 250 }}>
-					<Meta
-						title="Card title"
-						description="This is the description"
-						actions={[ <SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" /> ]}
-					/>
+			<div
+				className="BrowseStategiesCard"
+				style={{ display: 'flex', flexDirection: 'row' }}
+				onClick={console.log('Clicked: ' + this.props.title)}
+			>
+				<img src={this.props.imgPath} style={{ width: '130px', height: '130px' }} />
+				<Card
+					title={this.props.title}
+					extra={<Rate disabled allowHalf defaultValue={this.props.avgRating} />}
+					size="small"
+					style={{ width: '31.5rem', height: '130px' }}
+					headStyle={{ height: 0, fontSize: '14pt', border: 'none' }}
+				>
+					<Row>
+						<Col span={12}>
+							<Meta description={this.props.description} />
+						</Col>
+						<Col span={12} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+							<Meta description={`${this.props.subscriberCount} Subscribers`} style={{ marginBottom: '0.1rem' }} />
+							<Meta description={`${this.props.ratingCount} Ratings`} style={{ marginBottom: '0.1rem' }} />
+							<Button type="primary" size="small" style={{ marginTop: '0.4rem' }}>
+								Equip
+							</Button>
+						</Col>
+					</Row>
 				</Card>
 			</div>
 		)
