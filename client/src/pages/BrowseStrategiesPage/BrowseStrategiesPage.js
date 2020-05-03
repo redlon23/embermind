d7a0f923ca156028e05c02faf07159e2e9069b15
AdamDoquiatan <adamdoq@gmail.com>
2020-05-03 10:22:09 -0700

    BrowseStrategiesPage base layout

--- client/src/pages/BrowseStrategiesPage/BrowseStrategiesPage.js
+++ client/src/pages/BrowseStrategiesPage/BrowseStrategiesPage.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import validateSessionStatus from '../../sessionValidator'
 import HeaderNavBar from '../../sharedModules/HeaderNavBar/HeaderNavBar'
 import SideNavBar from '../../sharedModules/SideNavBar/SideNavBar'
+import PageTitleHeader from '../../sharedModules/PageTitleHeader/PageTitleHeader'
 
 import { Row, Col, Layout } from 'antd'
 
 const { Content } = Layout
 
-const contentStyle = { background: '#EBEBEB', border: '2px dashed blue' }
-const contentGutter = [ 28, { xs: 10, sm: 18, md: 26, lg: 34 } ]
+const contentStyle = {
+	background: '#1A1C25',
+	minHeight: '34.5rem',
+	fontSize: '16pt',
+	padding: '1rem',
+	color: '#EBEBEB'
+}
 
 class BrowseStrategiesPage extends Component {
 	async componentDidMount() {
@@ -23,7 +29,16 @@ class BrowseStrategiesPage extends Component {
 					<Layout>
 						<HeaderNavBar />
 						<Content style={{ padding: '2rem 2rem 0rem 2rem' }}>
-							<h1>Browse Strategies Page</h1>
+							<Row gutter={[ 28, { xs: 8, sm: 16, md: 24, lg: 32 } ]}>
+								<Col span={24}>
+									<PageTitleHeader header="Browse Strategies Page" />
+								</Col>
+							</Row>
+							<Row>
+								<Col span={24}>
+									<div style={contentStyle}>Stuff</div>
+								</Col>
+							</Row>
 						</Content>
 					</Layout>
 				</Layout>
