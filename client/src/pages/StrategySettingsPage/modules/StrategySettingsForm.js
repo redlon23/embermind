3e7378382bc33cc6789497ff0a46a7f8a07fffef
AdamDoquiatan <adamdoq@gmail.com>
2020-05-12 20:42:06 -0700

    fixed settings saving to state but being reset to initial values if moving from basic to advanced

--- client/src/pages/StrategySettingsPage/modules/StrategySettingsForm.js
+++ client/src/pages/StrategySettingsPage/modules/StrategySettingsForm.js
@@ -22,13 +22,108 @@ const tailLayout = {
 
 const fieldStyle = { width: '15rem' }
 
+const numIntInputRegEx = (value) => value.replace(/[^0-9]/, '')
+const numDecInputRegEx = (value) => value.replace(/[^0-9.]/g, '') // doesn't prevent multiple decimals
+const camelToTitle = (camelCase) => camelCase.replace(/([A-Z])/g, (match) => ` ${match}`).replace(/^./, (match) => match.toUpperCase())
+
+const basicSettingsFields = (context) => (
+	<div>
+		<Row>
+			<Col span={12}>
+				<Form
+					className="form-section"
+					{...layout}
+					onFinish={context.updateStrategySettings}
+					initialValues={{
+						quantity: context.state.quantity,
+						takeProfit: context.state.takeProfit,
+						stopLoss: context.state.stopLoss
+					}}
+				>
+					<Form.Item className="form-group" name="quantity" label="Contract Quantity" onChange={context.handleSaveInputToState}>
+						<InputNumber parser={numIntInputRegEx} style={fieldStyle} />
+					</Form.Item>
+					<Form.Item className="form-group" name="takeProfit" label="Take Profit" onChange={context.handleSaveInputToState}>
+						<InputNumber placeholder={context.props.strategySettings.takeProfit} parser={numDecInputRegEx} style={fieldStyle} />
+					</Form.Item>
+					<Form.Item className="form-group" name="stopLoss" label="Stop Loss" onChange={context.handleSaveInputToState}>
+						<InputNumber parser={numDecInputRegEx} style={fieldStyle} />
+					</Form.Item>
+				</Form>
+			</Col>
+		</Row>
+		<Row justify="end">
+			<Button type="primary" htmlType="submit" onClick={context.updateStrategySettings} style={{ marginRight: '6.2rem' }}>
+				Submit
+			</Button>
+		</Row>
+		{/*<Col span={12}>
+				<Form.Item>
+					<CoinSelector
+						selectedCoins={this.state.selectedCoins}
+						supportedCoins={this.state.supportedCoins}
+						updateSelectedCoins={this.updateSelectedCoins.bind(this)}
+					/>
+				</Form.Item>
+			</Col> */}
+	</div>
+)
+
+const advancedSettingsFields = (context) => (
+	<Form className="form-section" {...layout} onFinish={context.updateStrategySettings}>
+		{context.advancedSettings.map(
+			(settingName, index) =>
+				index % 2 === 0
+					? settingRow(context, settingName, context.advancedSettings[index + 1] ? context.advancedSettings[index + 1] : null, index)
+					: null
+		)}
+		{context.advancedSettings.length > 0 ? (
+			<Form.Item {...tailLayout}>
+				<Button type="primary" htmlType="submit" onClick={context.updateStrategySettings}>
+					Submit
+				</Button>
+			</Form.Item>
+		) : null}
+	</Form>
+)
+
+const settingRow = (context, settingName1, settingName2, index) => (
+	<Row key={index}>
+		<Col span={12}>
+			<Form.Item
+				className="form-group"
+				name={settingName1}
+				label={camelToTitle(settingName1)}
+				key={settingName1}
+				onChange={context.handleSaveInputToState}
+			>
+				<InputNumber placeholder={context.props.strategySettings[settingName1]} parser={numIntInputRegEx} style={fieldStyle} />
+			</Form.Item>
+		</Col>
+		<Col span={10}>
+			{settingName2 ? (
+				<Form.Item
+					className="form-group"
+					name={settingName2}
+					label={camelToTitle(settingName2)}
+					key={settingName2}
+					onChange={context.handleSaveInputToState}
+				>
+					<InputNumber placeholder={context.props.strategySettings[settingName2]} parser={numIntInputRegEx} style={fieldStyle} />
+				</Form.Item>
+			) : null}
+		</Col>
+	</Row>
+)
+
 class StrategySettingsForm extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			displayCategory: 'basic',
-			supportedCoins: [],
-			selectedCoins: []
+			renderDataLoaded: false
+			// supportedCoins: [],
+			// selectedCoins: []
 		}
 
 		this.basicSettings = [ 'quantity', 'takeProfit', 'stopLoss' ]
@@ -39,22 +134,19 @@ class StrategySettingsForm extends Component {
 			.sort()
 
 		this.displayOptions = {
-			basic: this.basicSettingsFields(),
-			advanced: this.advancedSettingsFields(this.advancedSettings)
+			basic: basicSettingsFields,
+			advanced: advancedSettingsFields
 		}
 	}
 
 	componentDidMount() {
-		this.setState({ ...this.props.strategySettings })
+		this.setState({ ...this.props.strategySettings, renderDataLoaded: true })
 	}
 
-	// componentDidUpdate() {
-	// 	console.log('Selected Coins: ' + this.state.selectedCoins)
-	// }
-
-	numIntInputRegEx = (value) => value.replace(/[^0-9]/, '')
-	numDecInputRegEx = (value) => value.replace(/[^0-9.]/g, '') // doesn't prevent multiple decimals
-	camelToTitle = (camelCase) => camelCase.replace(/([A-Z])/g, (match) => ` ${match}`).replace(/^./, (match) => match.toUpperCase())
+	componentDidUpdate() {
+		// console.log('Selected Coins: ' + this.state.selectedCoins)
+		console.log(JSON.stringify(this.state))
+	}
 
 	handleSaveInputToState = (event) => {
 		// Ensures only numbers and decimals are saved to state
@@ -64,22 +156,9 @@ class StrategySettingsForm extends Component {
 	}
 
 	updateStrategySettings = async () => {
-		const updatedSettings = {
-			strategyName: this.state.strategyName,
-			quantity: this.state.quantity,
-			DCA: this.state.DCA,
-			maxContractSize: this.state.maxContractSize,
-			noTradingZoneSize: this.state.noTradingZoneSize,
-			noTradingZoneRange: this.state.noTradingZoneRange,
-			numOrders: this.state.numOrders,
-			orderSpread: this.state.orderSpread,
-			spread: this.state.spread,
-			stopLoss: this.state.stopLoss,
-			takeProfit: this.state.takeProfit,
-			tradeInterval: this.state.tradeInterval,
-			trailingSafety: this.state.trailingSafety,
-			trailingStop: this.state.trailingStop
-		}
+		const updatedSettings = this.state
+		delete updatedSettings.displayCategory
+
 		const response = await fetch('/api/updateStrategySettings', {
 			method: 'POST',
 			headers: {
@@ -99,105 +178,28 @@ class StrategySettingsForm extends Component {
 	// 	this.setState({ selectedCoins: newCoins })
 	// }
 
-	basicSettingsFields = () => (
-		<div>
-			<Row>
-				<Col span={12}>
-					<Form className="form-section" {...layout} onFinish={this.updateStrategySettings}>
-						<Form.Item className="form-group" name="quantity" label="Contract Quantity" onChange={this.handleSaveInputToState}>
-							<InputNumber placeholder={this.props.strategySettings.quantity} parser={this.numIntInputRegEx} style={fieldStyle} />
-						</Form.Item>
-						<Form.Item className="form-group" name="takeProfit" label="Take Profit" onChange={this.handleSaveInputToState}>
-							<InputNumber placeholder={this.props.strategySettings.takeProfit} parser={this.numDecInputRegEx} style={fieldStyle} />
-						</Form.Item>
-						<Form.Item className="form-group" name="stopLoss" label="Stop Loss" onChange={this.handleSaveInputToState}>
-							<InputNumber placeholder={this.props.strategySettings.stopLoss} parser={this.numDecInputRegEx} style={fieldStyle} />
-						</Form.Item>
-					</Form>
-				</Col>
-			</Row>
-			<Row justify="end">
-				<Button type="primary" htmlType="submit" onClick={this.updateStrategySettings} style={{ marginRight: '6.2rem' }}>
-					Submit
-				</Button>
-			</Row>
-			{/*<Col span={12}>
-				<Form.Item>
-					<CoinSelector
-						selectedCoins={this.state.selectedCoins}
-						supportedCoins={this.state.supportedCoins}
-						updateSelectedCoins={this.updateSelectedCoins.bind(this)}
-					/>
-				</Form.Item>
-			</Col> */}
-		</div>
-	)
-
-	advancedSettingsFields = (advancedSettings) => (
-		<Form className="form-section" {...layout} onFinish={this.updateStrategySettings}>
-			{advancedSettings.map(
-				(settingName, index) =>
-					index % 2 === 0 ? this.settingRow(settingName, advancedSettings[index + 1] ? advancedSettings[index + 1] : null, index) : null
-			)}
-			{advancedSettings.length > 0 ? (
-				<Form.Item {...tailLayout}>
-					<Button type="primary" htmlType="submit" onClick={this.updateStrategySettings}>
-						Submit
-					</Button>
-				</Form.Item>
-			) : null}
-		</Form>
-	)
-
-	settingRow = (settingName1, settingName2, index) => (
-		<Row key={index}>
-			<Col span={12}>
-				<Form.Item
-					className="form-group"
-					name={settingName1}
-					label={this.camelToTitle(settingName1)}
-					key={settingName1}
-					onChange={this.handleSaveInputToState}
-				>
-					<InputNumber placeholder={this.props.strategySettings[settingName1]} parser={this.numIntInputRegEx} style={fieldStyle} />
-				</Form.Item>
-			</Col>
-			<Col span={10}>
-				{settingName2 ? (
-					<Form.Item
-						className="form-group"
-						name={settingName2}
-						label={this.camelToTitle(settingName2)}
-						key={settingName2}
-						onChange={this.handleSaveInputToState}
-					>
-						<InputNumber placeholder={this.props.strategySettings[settingName2]} parser={this.numIntInputRegEx} style={fieldStyle} />
-					</Form.Item>
-				) : null}
-			</Col>
-		</Row>
-	)
-
 	render() {
 		return (
 			<div className="StrategySettingsForm">
-				<Layout style={{ height: '28rem', marginTop: '0.5rem' }}>
-					<Sider>
-						<Menu theme="dark" mode="inline" defaultSelectedKeys={[ 'basic' ]}>
-							<Menu.Item key="basic" onClick={() => this.setState({ displayCategory: 'basic' })}>
-								Basic
-							</Menu.Item>
-							<Menu.Item key="advanced" onClick={() => this.setState({ displayCategory: 'advanced' })}>
-								Advanced
-							</Menu.Item>
-						</Menu>
-					</Sider>
-					<Content>
-						<div className="site-layout-background" style={{ paddingTop: 24, paddingBottom: 24, margin: 0, height: '28rem' }}>
-							{this.displayOptions[this.state.displayCategory]}
-						</div>
-					</Content>
-				</Layout>
+				{this.state.renderDataLoaded ? (
+					<Layout style={{ height: '28rem', marginTop: '0.5rem' }}>
+						<Sider>
+							<Menu theme="dark" mode="inline" defaultSelectedKeys={[ 'basic' ]}>
+								<Menu.Item key="basic" onClick={() => this.setState({ displayCategory: 'basic' })}>
+									Basic
+								</Menu.Item>
+								<Menu.Item key="advanced" onClick={() => this.setState({ displayCategory: 'advanced' })}>
+									Advanced
+								</Menu.Item>
+							</Menu>
+						</Sider>
+						<Content>
+							<div className="site-layout-background" style={{ paddingTop: 24, paddingBottom: 24, margin: 0, height: '28rem' }}>
+								{this.displayOptions[this.state.displayCategory](this)}
+							</div>
+						</Content>
+					</Layout>
+				) : null}
 			</div>
 		)
 	}
