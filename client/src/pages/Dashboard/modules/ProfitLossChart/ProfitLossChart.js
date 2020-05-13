1ed57272622211df3c2918f902f56b1148fdd114
Jason <jasonsbtang@gmail.com>
2020-05-13 15:55:52 -0700

    states for pnl chart, fixed invisible X axis

--- client/src/pages/Dashboard/modules/ProfitLossChart/ProfitLossChart.js
+++ client/src/pages/Dashboard/modules/ProfitLossChart/ProfitLossChart.js
@@ -3,6 +3,17 @@ import Chart from 'chart.js'
 import { Card } from 'antd'
 
 export default class ProfitLossChart extends Component {
+    state = {
+        dailyProfit: [19, 15, 24, 8, 11, 10, 18],
+        dailyLoss: [12, 13, 20, 5, 9, 7, 15],
+        dailyLabels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        weeklyProfit: [19, 15, 24, 8, 11, 10, 18, 19],
+        weeklyLoss: [12, 13, 20, 5, 9, 7, 15, 14],
+        weeklyLabels: ['3/29-4/4', '4/5-4/11', '4/12-418', '4/19-4/25', '4/26-w/e', 'w/e-n/a', 'ya-na', 'hm-ha'],
+        monthlyProfit: [19, 15, 24, 8, 11, 10, 18, 19, 12, 17, 24, 13],
+        monthlyLoss: [12, 13, 20, 5, 9, 7, 15, 14, 8, 15, 19, 10],
+        monthlyLabels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    }
     chartRef = React.createRef();
 
     componentDidMount(){
@@ -13,19 +24,19 @@ export default class ProfitLossChart extends Component {
         new Chart(myChartRef, {
             type: 'bar',
             data: {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                labels: this.state.weeklyLabels,
                 datasets: [{
                     label: "Profit",
                     barPercentage: 0.2,
                     categoryPercentage: 0.5,
-                    data: [19, 15, 24, 8, 11, 10, 18, 19, 12, 17, 24, 13],
+                    data: this.state.weeklyProfit,
                     backgroundColor: "rgba(0, 255, 0 , 1)",
                 },
                 {
                     label: "Loss",
                     barPercentage: 0.2,
                     categoryPercentage: 0.5,
-                    data: [12, 13, 20, 5, 9, 7, 15, 14, 8, 15, 19, 10],
+                    data: this.state.weeklyLoss,
                     backgroundColor: "rgba(255, 0, 0 , 1)",
                 }]
             },
@@ -47,7 +58,7 @@ export default class ProfitLossChart extends Component {
                     yAxes: [{
                         gridLines: {
                             color: '#555555',
-                            zeroLineWidth: 0,
+                            zeroLineColor: '#555555',
                             drawBorder: false
                         },
                         ticks: {beginAtZero: true}
