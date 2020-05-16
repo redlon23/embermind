51b5cf2313ff13c760a0a2c6d81f81f186d1d131
AdamDoquiatan <adamdoq@gmail.com>
2020-05-15 17:32:51 -0700

    UI SideNavBar

--- client/src/sharedModules/SideNavBar/SideNavBar.js
+++ client/src/sharedModules/SideNavBar/SideNavBar.js
@@ -31,21 +31,20 @@ class SideNavBar extends Component {
 
 	render() {
 		return (
-			<div className="SideNavBar">
-				<Sider style={{ minHeight: '100vh' }}>
-					<div className="logo-container">
-						<img
-							className="logo"
-							src={`${process.env.PUBLIC_URL}logo-text.png`}
-							alt="EmberMind"
-							onClick={() => this.props.history.push('/')}
-						/>
-					</div>
-					<div className="user-container">
-						<Button type="link" className="user-name" onClick={() => this.props.history.push('/account-settings')}>
-							{cookies.get('name')}
-						</Button>
-						{/*}
+			<Sider className="SideNavBar" style={{ minHeight: '100vh' }}>
+				<div className="logo-container">
+					<img
+						className="logo"
+						src={`${process.env.PUBLIC_URL}logo-text.png`}
+						alt="EmberMind"
+						onClick={() => this.props.history.push('/')}
+					/>
+				</div>
+				<div className="user-container">
+					<Button type="link" className="user-name" onClick={() => this.props.history.push('/account-settings')}>
+						{cookies.get('name')}
+					</Button>
+					{/*}
 						<Badge count={1}>
 							<Avatar
 								className="notification-icon"
@@ -55,36 +54,35 @@ class SideNavBar extends Component {
 							/>
 						</Badge>
 				*/}
-					</div>
-					<Menu theme="dark" mode="inline" defaultSelectedKeys={[ this.props.location.pathname ]}>
-						<Menu.Divider />
-						<Menu.Item key="/dashboard" onClick={() => this.props.history.push('/dashboard')}>
-							<span className="nav-text">Dashboard</span>
-						</Menu.Item>
-						<Menu.Item key="/trade-records" disabled={true}>
-							<span className="nav-text">Trade Records</span>
-						</Menu.Item>
-						<Menu.Item key="/equipped-strategies" disabled={true}>
-							<span className="nav-text">Equipped Strategies</span>
-						</Menu.Item>
-						<Menu.Divider />
-						<Menu.Item key="/account-settings" onClick={() => this.props.history.push('/account-settings')}>
-							<span className="nav-text">Account Settings</span>
-						</Menu.Item>
-						<Menu.Item key="/strategy-settings" onClick={() => this.props.history.push('/strategy-settings')}>
-							<span className="nav-text">Strategy Settings</span>
-						</Menu.Item>
-					</Menu>
-					<Menu theme="dark" selectable={false}>
-						<Menu.Item>
-							<Switch defaultChecked={this.state.tradingEnabled === 'true'} onChange={this.toggleTrading} />
-							<span className="nav-text" style={{ paddingLeft: '0.6rem' }}>
-								Enable Trading
-							</span>
-						</Menu.Item>
-					</Menu>
-				</Sider>
-			</div>
+				</div>
+				<Menu className="navMenu" mode="inline" defaultSelectedKeys={[ this.props.location.pathname ]}>
+					<Menu.Divider />
+					<Menu.Item key="/dashboard" onClick={() => this.props.history.push('/dashboard')}>
+						<span className="navText">Dashboard</span>
+					</Menu.Item>
+					<Menu.Item key="/trade-records" disabled={true}>
+						<span className="disabledText">Trade Records</span>
+					</Menu.Item>
+					<Menu.Item key="/equipped-strategies" disabled={true}>
+						<span className="disabledText">Equipped Strategies</span>
+					</Menu.Item>
+					<Menu.Divider />
+					<Menu.Item key="/account-settings" onClick={() => this.props.history.push('/account-settings')}>
+						<span className="nav-text">Account Settings</span>
+					</Menu.Item>
+					<Menu.Item key="/strategy-settings" onClick={() => this.props.history.push('/strategy-settings')}>
+						<span className="nav-text">Strategy Settings</span>
+					</Menu.Item>
+				</Menu>
+				<Menu className="enableTradingToggle" selectable={false}>
+					<Menu.Item>
+						<Switch defaultChecked={this.state.tradingEnabled === 'true'} onChange={this.toggleTrading} />
+						<span className="nav-text" style={{ paddingLeft: '0.6rem' }}>
+							Enable Trading
+						</span>
+					</Menu.Item>
+				</Menu>
+			</Sider>
 		)
 	}
 }
